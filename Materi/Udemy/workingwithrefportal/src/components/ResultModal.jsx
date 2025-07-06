c91db06ee8db0ad638161732e4cea016b7aac082
Martinus Minggu <900511@hq.bni.co.id>
2025-07-06 22:32:00 +0700

    menambhkan forwaderref

---
+++ Materi/Udemy/workingwithrefportal/src/components/ResultModal.jsx
@@ -0,0 +1,12 @@
+import { forwardRef } from "react"
+
+export default forwardRef(function ResultModal({ result, targetTime }, ref) {
+    return <dialog ref={ref} className="result-modal"  >
+        <h2>You {result}</h2>
+        <p>The target time  was <strong>{targetTime} seconds</strong></p>
+        <p>You stopped the timer with <strong>{targetTime} seconds left.</strong></p>
+        <form method="dialog">
+            <button>Close</button>
+        </form>
+    </dialog>
+});
