cf92f0b1e17b381b30a0901ce6d696cae45a1c4e
Martinus Minggu <900511@hq.bni.co.id>
2025-07-07 17:46:38 +0700

    setengah progres

--- Materi/Udemy/workingwithrefportal/src/components/TimerChallenges.jsx
+++ Materi/Udemy/workingwithrefportal/src/components/TimerChallenges.jsx
@@ -2,29 +2,39 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 // let timer;
 export default function TimerChallenge({ title, targetTime }) {
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
+
+
+    /** 
+     * Logic nya
+     * 1. jika user terlambat menekan stop dari waktu yang ditentukan maka dia akan kalah
+     * 2. jika user menekan stop antara waktu target dan 0 maka dianggap menang
+     * 3. data yang ditampilkan adalah
+     *  - dia menang atau kalah
+     *  - waktu yang tersisa
+     *  - scorenya berapa 
+    */
     const timer = useRef();
     const dialog = useRef();
+
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+        setTimeRemaining(targetTime * 1000);
+    }
     function handleStart() {
-        timer.current = setTimeout(() => {
-            setTimerExpired(() => true);
-            // setTimerStarted(() => false);
-            dialog.current.showModal();
-            // setTimerStarted(false);
-        }, targetTime * 1000);
-        setTimerStarted(() => true);
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeReamining => prevTimeReamining - 10)
+        }, 10);
     }
     function handleStop() {
-        clearTimeout(timer.current);
-        // setTimerStarted(() => false);
-
+        clearInterval(timer.current);
     }
     return <>
         <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
         <section className="challenge">
             <h2>{title}</h2>
-            {timerExpired && <p>You lost</p>}
+            {/* {timerExpired && <p>You lost</p>} */}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
