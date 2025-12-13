b25afe8fb9564a6e44745fa01def5007e7e798bc
Martinus Minggu <900511@hq.bni.co.id>
2025-12-13 22:52:44 +0700

    menambahkan error fetch data

--- Materi/Udemy/bagian15 ConnectingToDatabase/01-starting-project/01-starting-project/src/components/AvailablePlaces.jsx
+++ Materi/Udemy/bagian15 ConnectingToDatabase/01-starting-project/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,20 +1,41 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import { apiBackend, get } from '../common/commonApi.js';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoadingPlace, setIsLoadingPlace] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const api = apiBackend + '/places';
+    setIsLoadingPlace(true);
     // fetchApi(api, (data) => { setAvailablePlaces(data.places || []) });
-    get(api, data => setAvailablePlaces(data.places || []))
+    get(api, data => {
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const sortedPlaces = sortPlacesByDistance(data.places, position.coords.latitude, position.coords.longitude)
+        setAvailablePlaces(sortedPlaces || []);
+      })
+      setIsLoadingPlace(false);
+    }, () => setError("gagal mengambil data available places")
+    )
   }, []);
-  return (
+  if (error) {
+    console.log("error : " + error)
+    return <Error title="Error" message={error} />
+  }
+  return (<>
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoadingPlace}
+      loadingText="sedang mengambil data"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
+  </>
   );
 }
